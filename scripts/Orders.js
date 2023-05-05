cf1ae75c4bebded6312292af42f04770668ebd83
Sarah Saturday <sarahsaturday@gmail.com>
2023-05-05 15:37:00 -0500

    end of day push

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -7,7 +7,7 @@ const orders = getOrders()
 
 
 // Function whose responsibility is to find the product for an order
-const findproduct = (order, allProducts) => {
+const findProduct = (order, allProducts) => {
     let orderProduct = null
 
     for (const product of allProducts) {
@@ -20,10 +20,10 @@ const findproduct = (order, allProducts) => {
 }
 
 // Function whose responsibility is to find the employee for an order
-const findemployee = (order, allEmployees) => {
+const findEmployee = (order, allEmployees) => {
     let orderEmployee = null
 
-    for (const employee in allEmployees) {
+    for (const employee of allEmployees) {
         if (employee.id === order.employeeId) {
             orderEmployee = employee
         }
@@ -38,7 +38,7 @@ export const Orders = () => {
 
     for (const order of orders) {
         const employee = findEmployee(order, employees)
-        const product = findProduct(order)
+        const product = findProduct(order, products)
 
         html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
     }
--- scripts/Products.js
+++ scripts/Products.js
@@ -3,7 +3,7 @@ import { getProducts } from "./database.js"
 const products = getProducts()
 
 export const Products = () => {
-    let html = "<ul>`
+    let html = "<ul>"
 
     for (const product of products) {
         html += `<li>${product.title}</li>`
